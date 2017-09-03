f4c4756c29e155333b657e7e10b63dc2b0a3cb0b
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-09-03 20:33:18 +0530

    added date filter

--- src/app/service/product/product.service.ts
+++ src/app/service/product/product.service.ts
@@ -4,9 +4,9 @@ import { Product } from './product';
 @Injectable()
 export class ProductService {
   products: Product[] = [
-    { id: 1, name: 'Product1', price: 100, imageUrl: 'http://test.com' },
-    { id: 2, name: 'Product2', price: 2000, imageUrl: 'http://test.com' },
-    { id: 3, name: 'Product3', price: 3000, imageUrl: 'http://test.com' }
+    { id: 1, name: 'Product1', price: 100, imageUrl: 'http://test.com', createdDate: new Date('11-10-1987') },
+    { id: 2, name: 'Product2', price: 2000, imageUrl: 'http://test.com', createdDate: new Date('11-10-1987') },
+    { id: 3, name: 'Product3', price: 3000, imageUrl: 'http://test.com', createdDate: new Date('11-10-1987') }
   ];
   constructor() { }
 
@@ -15,6 +15,6 @@ export class ProductService {
   }
 
   addProduct() {
-    this.products.push({ id: 4, name: 'Product4', price: 4000, imageUrl: 'http://test.com' });
+    this.products.push({ id: 4, name: 'Product4', price: 4000, imageUrl: 'http://test.com', createdDate: new Date('11-10-1987') });
   }
 }
